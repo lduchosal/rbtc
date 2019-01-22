258c4dd3fa65bcba83e46abd791cd1fd1feddef8
Luc Duchosal <q@MacBook-Pro-de-Luc.local>
2019-01-22 07:46:58 +0100

    implementing rbtc cli

--- rbtc_cli/src/main.rs
+++ rbtc_cli/src/main.rs
@@ -1,14 +1,53 @@
 extern crate copperline;
 extern crate encoding;
 
+use std::process::exit;
 use copperline::Encoding::Utf8;
 use copperline::Copperline;
 
+
+enum Command {
+    Help,
+    Quit
+}
 fn main() {
 
     let mut cl = Copperline::new();
     while let Ok(line) = cl.read_line("rbtc> ", Utf8) {
-        println!("Line: {}", line);
+        
+        let command = parse(&line);
+        match command {
+            Ok(Command::Help) => help(),
+            Ok(Command::Quit) => quit(),
+            Err(s) => err(s),
+        };
+
         cl.add_history(line);
+    }
+}
+
+fn help() {
+    println!("rbtc 0.4.0 (q)");
+    println!(" - quit : exit from rbtc");
+    println!(" - help : this message");
+}
+
+fn quit() {
+    println!("rbtc: have fun!");
+    exit(0);
+}
+
+fn err(invalid: &str) {
+    println!("rbtc: {}: command not found", invalid);
+}
+
+fn parse(line: &str) -> Result<Command, &str> {
+
+    match line {
+        "quit" => Ok(Command::Quit),
+        "exit" => Ok(Command::Quit),
+        "?" => Ok(Command::Help),
+        "help" => Ok(Command::Help),
+        _ => Err(line),
     }
 }
